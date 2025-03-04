94a1c625ddcbf40f9981211ae4b9e14ac36cd79e
Sean <sean@noteflight.com>
2025-03-04 10:19:25 -0500

    redesigned the header, removed "Contact Me" button in favor of mail icon, update how job title on experience page wrapped, changed default font size on mobile

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const HEADER_HEIGHT = 60;
 
 window.addEventListener('scroll', function() {
   const { scrollY } = window;
-  this.document.querySelector('header').classList.toggle('darken', scrollY > HEADER_HEIGHT);
+  this.document.querySelector('header').classList.toggle('darken-header', scrollY > HEADER_HEIGHT);
 });
