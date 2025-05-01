001600ea174c04eb41a9e9aad0100d67d18e987b
Sean Nealon <signalflowsean@macbookpro.mynetworksettings.com>
2025-05-01 15:22:29 -0400

    current page state

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
-const HEADER_HEIGHT = 60;
+const setActiveNavItem = () => {
+  const currentPage = window.location.pathname.split('/').pop();
 
-window.addEventListener('scroll', function() {
-  const { scrollY } = window;
-  this.document.querySelector('header').classList.toggle('darken-header', scrollY > HEADER_HEIGHT);
-});
+  const PAGES = {
+    'index.html': 'home',
+    'experience.html': 'experience',
+    'projects.html': 'projects',
+  };
+
+  const activeNavId = PAGES[currentPage];
+
+  // If a matching nav-item ID is found, add the 'active' class
+  if (activeNavId) {
+    const activeNavItem = document.getElementById(activeNavId);
+    if (activeNavItem) {
+      activeNavItem.classList.add('rainbow-gradient-background');
+    }
+  }
+}
+
+// Call the function when the DOM is fully loaded
+document.addEventListener('DOMContentLoaded', setActiveNavItem);
